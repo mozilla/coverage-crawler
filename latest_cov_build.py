a961cb16170f75fe519d6a89a38059380a8465cb
Assiya Khuzyakhmetova <assiya.khuzyakhmetova@nu.edu.kz>
2018-03-14 00:58:41 +0600

    download the latest Firefox coverage build

--- latest_cov_build.py
+++ latest_cov_build.py
@@ -1,3 +1,4 @@
+<<<<<<< 835b3e8d057109aebc9eaf465a0e161a305c71f7
 import os
 import tarfile
 
@@ -17,4 +18,30 @@ urlretrieve('https://index.taskcluster.net/v1/task/gecko.v2.' +
 # Extracting artifact
 with tarfile.open(name, 'r:bz2') as tar:
     tar.extractall()
+=======
+import tarfile
+import sys
+import json
+import os
+from urllib import urlretrieve, urlencode
+from urllib2 import urlopen
+
+
+def get_json(url, params=None):
+    if params is not None:
+        url += '?' + urlencode(params)
+
+    r = urlopen(url).read().decode('utf-8')
+
+    return json.loads(r)
+
+last_task = get_json('https://index.taskcluster.net/v1/task/gecko.v2.mozilla-central.latest.firefox.linux64-ccov-opt')
+taskId = last_task['taskId']
+name = os.path.join('ccov-artifacts', taskId + 'artifacts.public.build.target.tar.bz2')
+urlretrieve('https://queue.taskcluster.net/v1/task/' + taskId + '/artifacts/public/build/target.tar.bz2', name)
+tar = tarfile.open(name, 'r:bz2')
+tar.extractall()
+tar.close()
+# target.tar.bz2 deleted
+>>>>>>> download the latest Firefox coverage build
 os.remove(name)
