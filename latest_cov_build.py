3311e63a7a4c4abb50002e1f0fdaf23445e52909
Assiya Khuzyakhmetova <assiya.khuzyakhmetova@nu.edu.kz>
2018-03-14 22:52:45 +0600

    fixed issues

--- latest_cov_build.py
+++ latest_cov_build.py
@@ -1,25 +1,20 @@
 import os
 import tarfile
 
-import taskcluster
-
 try:
     from urllib.request import urlretrieve
 except ImportError:
     from urllib import urlretrieve
 
 
-# Retrieving task from index
-index = taskcluster.Index()
-taskId = index.findTask('gecko.v2.mozilla-central.' +
-                        'latest.firefox.linux64-ccov-opt')['taskId']
-# taskId uniquelly identifies artifact
-name = os.path.join('ccov-artifacts', taskId +
-                    'artifacts.public.build.target.tar.bz2')
+name = os.path.join('ccov-artifacts',
+                    'latest.artifacts.public.build.target.tar.bz2')
+
 # Retrieving artifact
 urlretrieve('https://index.taskcluster.net/v1/task/gecko.v2.' +
             'mozilla-central.latest.firefox.linux64-ccov-opt/' +
             'artifacts/public/build/target.tar.bz2', name)
+
 # Extracting artifact
 with tarfile.open(name, 'r:bz2') as tar:
     tar.extractall()
