b44c3f0e17b7c1e2ce14e4e3e1f809ccdabf2472
Assiya Khuzyakhmetova <assiya.khuzyakhmetova@nu.edu.kz>
2018-03-14 22:20:53 +0600

    changed urlretreve arguments

--- latest_cov_build.py
+++ latest_cov_build.py
@@ -8,17 +8,18 @@ try:
 except ImportError:
     from urllib import urlretrieve
 
+
 # Retrieving task from index
 index = taskcluster.Index()
 taskId = index.findTask('gecko.v2.mozilla-central.' +
                         'latest.firefox.linux64-ccov-opt')['taskId']
-# Retrieving artifact from Queue
-queue = taskcluster.Queue()
-url = queue.buildUrl('getLatestArtifact', taskId,
-                     'public/build/target.tar.bz2')
+# taskId uniquelly identifies artifact
 name = os.path.join('ccov-artifacts', taskId +
                     'artifacts.public.build.target.tar.bz2')
-urlretrieve(url, name)
+# Retrieving artifact
+urlretrieve('https://index.taskcluster.net/v1/task/gecko.v2.' +
+            'mozilla-central.latest.firefox.linux64-ccov-opt/' +
+            'artifacts/public/build/target.tar.bz2', name)
 # Extracting artifact
 with tarfile.open(name, 'r:bz2') as tar:
     tar.extractall()
