74c32ba83942c91209a86270de258b8c53e325dc
Assiya Khuzyakhmetova <assiya.khuzyakhmetova@nu.edu.kz>
2018-03-14 14:41:14 +0600

    implemented with taskcluster API

--- latest_cov_build.py
+++ latest_cov_build.py
@@ -1,25 +1,26 @@
 import tarfile
 import sys
-import json
 import os
-from urllib import urlretrieve, urlencode
-from urllib2 import urlopen
+import taskcluster
+try:
+    from urllib.parse import urlencode
+    from urllib.request import urlopen, urlretrieve
+except ImportError:
+    from urllib import urlencode, urlretrieve
+    from urllib2 import urlopen
 
 
-def get_json(url, params=None):
-    if params is not None:
-        url += '?' + urlencode(params)
-
-    r = urlopen(url).read().decode('utf-8')
-
-    return json.loads(r)
-
-last_task = get_json('https://index.taskcluster.net/v1/task/gecko.v2.mozilla-central.latest.firefox.linux64-ccov-opt')
-taskId = last_task['taskId']
-name = os.path.join('ccov-artifacts', taskId + 'artifacts.public.build.target.tar.bz2')
-urlretrieve('https://queue.taskcluster.net/v1/task/' + taskId + '/artifacts/public/build/target.tar.bz2', name)
+index = taskcluster.Index()
+queue = taskcluster.Queue()
+route = "gecko.v2.mozilla-central.latest.firefox.linux64-ccov-opt"
+taskId = index.findTask(route)['taskId']
+artifactName = 'public/build/target.tar.bz2'
+name = os.path.join('ccov-artifacts', taskId +
+                    'artifacts.public.build.target.tar.bz2')
+url = queue.buildUrl('getLatestArtifact', taskId, artifactName)
+print(url)
+urlretrieve(url, name)
 tar = tarfile.open(name, 'r:bz2')
 tar.extractall()
 tar.close()
-# target.tar.bz2 deleted
 os.remove(name)
