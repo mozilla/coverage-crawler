8fd2203a2b60caa88c0f4c240137d88cddb1006d
Assiya Khuzyakhmetova <assiya.khuzyakhmetova@nu.edu.kz>
2018-03-14 20:26:39 +0600

    added context manager, checked with flake8

--- latest_cov_build.py
+++ latest_cov_build.py
@@ -1,26 +1,21 @@
 import tarfile
-import sys
 import os
 import taskcluster
 try:
-    from urllib.parse import urlencode
-    from urllib.request import urlopen, urlretrieve
+    from urllib.request import urlretrieve
 except ImportError:
-    from urllib import urlencode, urlretrieve
-    from urllib2 import urlopen
+    from urllib import urlretrieve
 
 
 index = taskcluster.Index()
+taskId = index.findTask('gecko.v2.mozilla-central.' +
+                        'latest.firefox.linux64-ccov-opt')['taskId']
 queue = taskcluster.Queue()
-route = "gecko.v2.mozilla-central.latest.firefox.linux64-ccov-opt"
-taskId = index.findTask(route)['taskId']
-artifactName = 'public/build/target.tar.bz2'
+url = queue.buildUrl('getLatestArtifact', taskId,
+                     'public/build/target.tar.bz2')
 name = os.path.join('ccov-artifacts', taskId +
                     'artifacts.public.build.target.tar.bz2')
-url = queue.buildUrl('getLatestArtifact', taskId, artifactName)
-print(url)
 urlretrieve(url, name)
-tar = tarfile.open(name, 'r:bz2')
-tar.extractall()
-tar.close()
+with tarfile.open(name, 'r:bz2') as tar:
+    tar.extractall()
 os.remove(name)
