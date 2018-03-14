86607f3555b1d47818b444f189a85c3d720e60c8
Assiya Khuzyakhmetova <assiya.khuzyakhmetova@nu.edu.kz>
2018-03-14 23:38:33 +0600

    Add a script to download the latest Firefox coverage build (#8)

---
+++ latest_cov_build.py
@@ -0,0 +1,21 @@
+import os
+import tarfile
+
+try:
+    from urllib.request import urlretrieve
+except ImportError:
+    from urllib import urlretrieve
+
+
+name = os.path.join('ccov-artifacts',
+                    'latest.artifacts.public.build.target.tar.bz2')
+
+# Retrieving artifact
+urlretrieve('https://index.taskcluster.net/v1/task/gecko.v2.' +
+            'mozilla-central.latest.firefox.linux64-ccov-opt/' +
+            'artifacts/public/build/target.tar.bz2', name)
+
+# Extracting artifact
+with tarfile.open(name, 'r:bz2') as tar:
+    tar.extractall()
+os.remove(name)
