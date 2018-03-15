f224f31aacef0ff0b926a1132ad45ca97e9b33df
Assiya <assiya.khuzyakhmetova@nu.edu.kz>
2018-03-15 23:39:14 +0600

    rebased

--- README.md
+++ README.md
@@ -1,11 +1,4 @@
 # coverage-crawler
-<<<<<<< 21515a03538f6d94b65ca1cbd42aa6c4afa0011e
-<<<<<<< 598b710e1a5c1e5544443e6ac461f9652b56c2bc
-=======
-
->>>>>>> Added Travis badge
-=======
->>>>>>> Update README.md
 [![Build Status](https://travis-ci.org/marco-c/coverage-crawler.svg?branch=master)](https://travis-ci.org/marco-c/coverage-crawler)
 
 A crawler to find websites that exercise code in Firefox that is not covered by unit tests
@@ -16,10 +9,3 @@ A crawler to find websites that exercise code in Firefox that is not covered by 
 - Install development requirements with `pip install -r test-requirements.txt`;
 - Run the `latest_cov_build.py` script to download the latest Firefox coverage build;
 - Run the `crawler.py` script.
-<<<<<<< 76739c15a29d026a1449e901f6156a8057126996
-<<<<<<< 6d7cb21b6390ed50a0c2a01aac0e6229d72efc1b
-=======
->>>>>>> readme
-
-=======
->>>>>>> Update README.md
--- latest_cov_build.py
+++ latest_cov_build.py
@@ -1,5 +1,3 @@
-<<<<<<< c37f3812229c1a1a3d771aa145c747a73b41cafb
-<<<<<<< 835b3e8d057109aebc9eaf465a0e161a305c71f7
 import os
 import tarfile
 
@@ -10,7 +8,6 @@ except ImportError:
 
 
 name = 'latest.artifacts.public.build.target.tar.bz2'
-<<<<<<< 8dc78ad82a97fc8bf7ceddab5d90a251851b3872
 
 # Retrieving artifact
 urlretrieve('https://index.taskcluster.net/v1/task/gecko.v2.' +
@@ -20,61 +17,4 @@ urlretrieve('https://index.taskcluster.net/v1/task/gecko.v2.' +
 # Extracting artifact
 with tarfile.open(name, 'r:bz2') as tar:
     tar.extractall()
-=======
-import tarfile
-=======
->>>>>>> flake isort imports cleanup
-import os
-import tarfile
-
-try:
-    from urllib.request import urlretrieve
-except ImportError:
-    from urllib import urlretrieve
-
-
-<<<<<<< 6bdff2dcd16cdc6a4f4437272c6e77eac0a31738
-# Retrieving task from index
-index = taskcluster.Index()
-taskId = index.findTask('gecko.v2.mozilla-central.' +
-                        'latest.firefox.linux64-ccov-opt')['taskId']
-# taskId uniquelly identifies artifact
-name = os.path.join('ccov-artifacts', taskId +
-                    'artifacts.public.build.target.tar.bz2')
-<<<<<<< 4c96a85728db505594bc213bd2608e280d40ebed
-urlretrieve(url, name)
-<<<<<<< 8b47a9f09fcbb0ec4d978d66b1061afde1554a0f
-<<<<<<< c43ebd63c26dd0a52d2fb192873e0cd6fdbbe52f
-tar = tarfile.open(name, 'r:bz2')
-tar.extractall()
-tar.close()
-<<<<<<< a961cb16170f75fe519d6a89a38059380a8465cb
-# target.tar.bz2 deleted
->>>>>>> download the latest Firefox coverage build
-=======
->>>>>>> implemented with taskcluster API
-=======
-=======
-=======
-=======
-name = os.path.join('ccov-artifacts',
-                    'latest.artifacts.public.build.target.tar.bz2')
-=======
->>>>>>> Update latest_cov_build.py
-
->>>>>>> fixed issues
-# Retrieving artifact
-urlretrieve('https://index.taskcluster.net/v1/task/gecko.v2.' +
-            'mozilla-central.latest.firefox.linux64-ccov-opt/' +
-            'artifacts/public/build/target.tar.bz2', name)
-<<<<<<< 6bdff2dcd16cdc6a4f4437272c6e77eac0a31738
->>>>>>> changed urlretreve arguments
-=======
-
->>>>>>> fixed issues
-# Extracting artifact
->>>>>>> sections added
-with tarfile.open(name, 'r:bz2') as tar:
-    tar.extractall()
->>>>>>> added context manager, checked with flake8
 os.remove(name)
