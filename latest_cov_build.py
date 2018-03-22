de226734aef6b91a88bf7195c286000dd9459cd1
Assiya <assiya.khuzyakhmetova@nu.edu.kz>
2018-03-22 17:25:37 +0600

    grcov download
    grcov
    Automatically download grcov
    
    flake8
    
    added changes
    grcov file download and code reorganization
    
    Restructured and cleaned the code
    
    Restructured and cleaned the code
    Cleaned code and applied changes
    
    Cleaned the code
    GRCOV Download and applied changes

--- latest_cov_build.py
+++ latest_cov_build.py
@@ -13,6 +13,19 @@ except ImportError:
     from urllib import urlretrieve
 
 
+def get_github_release_url(repo_slug):
+    repos_url = 'https://api.github.com/repos/{}/releases/latest'.format(repo_slug)
+    download_url = 'https://github.com/{}/releases/download/'.format(repo_slug)
+    latest_version = requests.get(repos_url)
+    data = latest_version.json()
+    tag_name = data['tag_name']
+    return download_url, tag_name
+
+
+# Create 'tools/' directory if doesn't exist
+if not os.path.exists('tools'):
+    os.makedirs('tools')
+
 index = taskcluster.Index()
 queue = taskcluster.Queue()
 
@@ -24,54 +37,53 @@ for name in ['target.tar.bz2', 'target.code-coverage-gcno.zip', 'chrome-map.json
     url = queue.buildUrl('getLatestArtifact', taskId, 'public/build/{}'.format(name))
     urlretrieve(url, os.path.join('tools', name))
 
-# Extracting coverage build artifact
-with tarfile.open('tools/target.tar.bz2', 'r:bz2') as tar:
-    tar.extractall(path='tools')
-os.remove('tools/target.tar.bz2')
+# Geckodriver base url fot the latest version
+download_url, tag_name = get_github_release_url('mozilla/geckodriver')
+geckodriver_url = download_url + tag_name + '/geckodriver-' + tag_name + '-'
 
-# Geckodriver download
+# Grcov latest version base url
+download_url, tag_name = get_github_release_url('marco-c/grcov')
+grcov_url = download_url + tag_name
+
 # OS information for correct geckodriver version
 bitness = platform.architecture()[0]
-latest_version = requests.get('https://api.github.com/repos/mozilla/geckodriver/releases/latest')
-data = latest_version.json()
-tag_name = data['tag_name']
-base_url = 'https://github.com/mozilla/geckodriver/releases/download/' + tag_name + '/geckodriver-' + tag_name + '-'
 
-# Linux OS
+# Complete urls according to platforms
 if sys.platform.startswith('linux'):
+    grcov_url += '/grcov-linux-x86_64.tar.bz2'
     if bitness == '64bit':
         version = 'linux64.tar.gz'
     else:
         version = 'linux32.tar.gz'
-
-# MacOS
 elif sys.platform.startswith('darwin'):
+    grcov_url += '/grcov-osx-x86_64.tar.bz2'
     version = 'macos.tar.gz'
-
-# Windows or Cygwin
 elif sys.platform.startswith('cygwin') or sys.platform.startswith('win32'):
+    grcov_url += '/grcov-win-x86_64.tar.bz2'
     if bitness == '64bit':
         version = 'win64.zip'
     else:
         version = 'win32.zip'
 
-# Create 'tools/' directory if doesn't exist
-if not os.path.exists('tools'):
-    os.makedirs('tools')
+# Download geckodriver
+geckodriver_archive = os.path.join('tools', version)
+geckodriver_url += version
+urlretrieve(geckodriver_url, geckodriver_archive)
 
-# Download
-name = os.path.join('tools', version)
-base_url += version
-urlretrieve(base_url, name)
+# Download grcov
+grcov_archive = os.path.join('tools', 'grcov.tar.bz2')
+urlretrieve(grcov_url, grcov_archive)
 
-# Extract
-if version.endswith('zip'):
-    with zipfile.ZipFile(name, 'r') as zip_ref:
-        zip_ref.extractall('tools')
-
-# MacOS and Linux OS have 'tar.gz' extensions
-elif base_url.endswith('tar.gz'):
-    with tarfile.open(name, 'r:gz') as tar:
-        tar.extractall(path='tools')
-
-os.remove(name)
+# Extract and delete archives for artifacts
+for filename in ['tools/target.code-coverage-gcno.zip', 'tools/target.tar.bz2', geckodriver_archive, grcov_archive]:
+    if filename.endswith('zip'):
+        with zipfile.ZipFile(filename, 'r') as zip_ref:
+            zip_ref.extractall(path='tools')
+    elif filename.endswith('tar.gz') or filename.endswith('tar.bz2'):
+        if filename.endswith('tar.gz'):
+            mode = 'r:gz'
+        else:
+            mode = 'r:bz2'
+        with tarfile.open(filename, mode) as tar:
+            tar.extractall(path='tools')
+    os.remove(filename)
