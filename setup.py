9d18196b8d896edfad5d76ebdac23a0a8c78cbdd
Assiya <assiya.khuzyakhmetova@nu.edu.kz>
2018-07-27 22:15:45 +0600

    updated function

--- setup.py
+++ setup.py
@@ -1,20 +1,34 @@
 # -*- coding: utf-8 -*-
 
+from __future__ import absolute_import
+
 import os
 
 from setuptools import find_packages
 from setuptools import setup
 
 
-def load_requirements(filename):
-    with open(os.path.join(os.path.dirname(__file__), filename)) as f:
-        return f.read().strip().split('\n')
+def read_requirements(file_):
+    lines = []
+    with open(file_) as f:
+        for line in f.readlines():
+            line = line.strip()
+            if line.startswith('-e ') or line.startswith('http://') or line.startswith('https://'):
+                extras = ''
+                if '[' in line:
+                    extras = '[' + line.split('[')[1].split(']')[0] + ']'
+                line = line.split('#')[1].split('egg=')[1] + extras
+            elif line == '' or line.startswith('#') or line.startswith('-'):
+                continue
+            line = line.split('#')[0].strip()
+            lines.append(line)
+    return sorted(list(set(lines)))
 
 
 setup(
     name='coverage_crawler',
     version='1.0.0',
     description='A crawler to find websites that exercise code in Firefox that is not covered by unit tests',
-    install_requires=load_requirements('requirements.txt'),
+    install_requires=read_requirements('requirements.txt'),
     packages=find_packages(exclude=['contrib', 'docs', 'tests'])
 )
