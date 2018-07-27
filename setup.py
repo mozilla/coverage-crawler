f6620b07a45370923a8bf68205d2a6cac03071e1
Assiya <assiya.khuzyakhmetova@nu.edu.kz>
2018-07-27 21:17:45 +0600

    Add requirements to setup.py

--- setup.py
+++ setup.py
@@ -7,5 +7,6 @@ setup(
     name='coverage_crawler',
     version='1.0.0',
     description='A crawler to find websites that exercise code in Firefox that is not covered by unit tests',
+    install_requires=load_requirements('requirements.txt'),
     packages=find_packages(exclude=['contrib', 'docs', 'tests'])
 )
