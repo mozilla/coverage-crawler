4d84f3458b17d0fa421f740cf94790d749876c18
Assiya <assiya.khuzyakhmetova@nu.edu.kz>
2018-08-03 14:02:50 +0600

    WIP: Add a script to upload reports to GitHub

--- run_crawler.py
+++ run_crawler.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
 
 from coverage_crawler import crawler
+from coverage_crawler import github
 
 with open('websites.txt') as f:
     for website in f:
         report = crawler.run(website)
+        github.upload_to_github(report)
