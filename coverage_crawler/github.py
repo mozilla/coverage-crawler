17c23e4f1ef20111af92df1e0a69619c2661dfd6
Assiya <assiya.khuzyakhmetova@nu.edu.kz>
2018-08-03 19:10:53 +0600

    Add changes to github.py

--- coverage_crawler/github.py
+++ coverage_crawler/github.py
@@ -5,27 +5,28 @@ import shutil
 import subprocess
 
 
-def upload_to_github(report_path):
+def upload_to_github(report_path, git_user_name, git_password):
     # Clone the repository if doesn't exist
     if not os.path.isdir('coverage-crawler-reports'):
-        subprocess.run(['git', 'clone', 'https://github.com/coverage-crawler-updater/coverage-crawler-reports'])
+        subprocess.run(['git', 'clone', 'https://github.com/rhcu/coverage-crawler-reports'])
     os.chdir('coverage-crawler-reports')
 
     # Remove the content of repository except of README
     prev_files = os.listdir(os.getcwd())
     for f in prev_files:
-        if f != 'README.md' and f.startswith('.') is False:
-            print(f)
+        if f != 'home' and f.startswith('.') is False:
+            os.remove(f)
+        if f == 'home':
+            shutil.rmtree('home')
     subprocess.run(['git', 'pull', 'https://github.com/rhcu/coverage-crawler-reports', 'master'])
 
     # Push the new content
     files = os.listdir(os.path.abspath(report_path))
-    print(files)
+
     for f in files:
-        print('Im here')
         shutil.move(os.path.join(report_path, f), os.getcwd())
 
     # Commit the content
     subprocess.run(['git', 'add', '*'])
     subprocess.run(['git', 'commit', '-m', 'Coverage crawler reports upload'])
-    subprocess.run(['git', 'push', 'https://coverage-crawler-updater:ACCESS_TOKEN@github.com/coverage-crawler-updater/coverage-crawler-reports', 'master', '--force'])
+    subprocess.run(['git', 'push', 'https://{}:{}@github.com/rhcu/coverage-crawler-reports'.format(git_user_name, git_password), 'master', '--force'])
--- run_crawler.py
+++ run_crawler.py
@@ -1,9 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from coverage_crawler import crawler
-from coverage_crawler import github
 
 with open('websites.txt') as f:
     for website in f:
         report = crawler.run(website)
-        github.upload_to_github(report)
