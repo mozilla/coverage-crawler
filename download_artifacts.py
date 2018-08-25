175de21e0c99ab9e59456d21673f58a74e54d8cb
Assiya Khuzyakhmetova <assiya.khuzyakhmetova@nu.edu.kz>
2018-08-25 23:12:55 +0600

    Move argsparse interface

--- download_artifacts.py
+++ download_artifacts.py
@@ -1,5 +1,10 @@
 # -*- coding: utf-8 -*-
 
+import argparse
+
 from coverage_crawler import latest_cov_build
 
-latest_cov_build.download_artifacts()
+parser = argparse.ArgumentParser()
+parser.add_argument('--revision', action='store', nargs='?', help='Optional revision of the build')
+args = parser.parse_args()
+latest_cov_build.download_artifacts(args.revision)
