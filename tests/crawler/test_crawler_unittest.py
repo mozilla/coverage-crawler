558b6ab0f9c1f2da2e7863b50c24ab06bfd84145
NNRepos <nivimano@gmail.com>
2022-10-14 11:19:24 +0300

    added crawler test
    also, new requirement for testing

---
+++ tests/crawler/test_crawler_unittest.py
@@ -0,0 +1,29 @@
+import unittest
+
+from selenium import webdriver
+from webdriver_manager.firefox import GeckoDriverManager
+
+from coverage_crawler import crawler
+
+
+class TestCrawler(unittest.TestCase):
+    driver: webdriver.Firefox
+
+    def setUp(self):
+        self.driver = webdriver.Firefox(executable_path=GeckoDriverManager().install())
+
+    def tearDown(self):
+        self.driver.close()
+
+    def test_close_all_windows_except_first(self):
+        """
+        when multiple windows are open,
+        verify that calling `close_all_windows_except_first` closes all except the first. 
+        """
+        _open_google_tab_script = 'window.open("https://google.com", "new window")'
+        self.driver.execute_script(_open_google_tab_script)
+        assert (len(self.driver.window_handles) == 2), "some windows were not opened properly."
+
+        crawler.close_all_windows_except_first(self.driver)
+
+        assert (len(self.driver.window_handles) == 1), "some windows were not closed properly."
