dc22c5429f2e097af7518bdc12a1f4062b5a6518
NNRepos <nivimano@gmail.com>
2022-10-21 11:24:27 +0300

    test `find_children` using flask server
    add class for tests which require local server
    fix flake8

--- tests/crawler/test_crawler_unittest.py
+++ tests/crawler/test_crawler_unittest.py
@@ -1,10 +1,14 @@
 # encoding: utf-8
+import multiprocessing
 import unittest
+from time import sleep
 
 from selenium import webdriver
 from webdriver_manager.firefox import GeckoDriverManager
 
 from coverage_crawler import crawler
+from tests.example_website import website_app
+from tests.example_website.website_app import run_server
 
 
 class TestCrawler(unittest.TestCase):
@@ -28,3 +32,38 @@ class TestCrawler(unittest.TestCase):
         crawler.close_all_windows_except_first(self.driver)
 
         assert (len(self.driver.window_handles) == 1), 'some windows were not closed properly.'
+
+
+class TestCrawlerLive(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.server = multiprocessing.Process(target=run_server)
+        cls.server.start()
+        sleep(1)
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.server.terminate()
+        cls.server.join()
+
+    def setUp(self):
+        self.driver = webdriver.Firefox(executable_path=GeckoDriverManager().install())
+
+    def tearDown(self):
+        self.driver.close()
+
+    def test_find_children(self):
+        """
+        when example server is open,
+        verify that find_children returns the expected children.
+        """
+        expected_title = 'coverage-crawler example website'
+        expected_link_text = 'i am the first link in the first div'
+
+        self.driver.get(website_app.WEBSITE_URL)
+        assert (self.driver.title == expected_title), f'incorrect driver title: {self.driver.title}'
+
+        children = crawler.find_children(self.driver)
+        num_children = len(children)
+        assert (num_children == 1), f'incorrect number of children found: {num_children}'
+        assert (children[0].text == expected_link_text)
---
+++ tests/example_website/website_app.py
@@ -0,0 +1,21 @@
+# encoding: utf-8
+import flask
+from flask import render_template
+
+WEBSITE_DOMAIN = 'localhost'
+WEBSITE_PORT = 8000
+WEBSITE_URL = f'{WEBSITE_DOMAIN}:{WEBSITE_PORT}'
+app = flask.Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+def run_server(debug=False):
+    app.run(host=WEBSITE_DOMAIN, port=WEBSITE_PORT, debug=debug)
+
+
+if __name__ == '__main__':
+    run_server(debug=True)
